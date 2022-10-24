bd517f38cd8b24c2f10bc9d8bdb83a8f31ffffdf
Taslim <taslimahmed313@gmil.com>
2022-10-25 01:36:48 +0600

    Navber requirments added

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Container, Nav, Navbar } from 'react-bootstrap';
+import { Container, Image, Nav, Navbar } from 'react-bootstrap';
+import logo from '../../images/coding-language.png';
 import './Header.css';
           
 const Header = () => {
@@ -8,12 +9,19 @@ const Header = () => {
         <div className="header">
           <Navbar bg="dark" expand="lg">
             <Container>
+              <Image src={logo} style={{width:'30px', marginRight:'20px'}}></Image>
               <Navbar.Brand href="#home">Bytecode Clouds</Navbar.Brand>
               <Navbar.Toggle aria-controls="basic-navbar-nav" />
               <Navbar.Collapse id="basic-navbar-nav">
                 <Nav className="me-auto">
-                  <Nav.Link href="#home">Home</Nav.Link>
-                  <Nav.Link href="#link">Link</Nav.Link>
+                  <Nav.Link href="#home">Courses</Nav.Link>
+                  <Nav.Link href="#link">FAQ</Nav.Link>
+                  <Nav.Link href="#link">Blog</Nav.Link>
+                  <Nav.Link href="#link">Dark</Nav.Link>
+                  <Nav.Link href="#link">Light</Nav.Link>
+                  <Nav.Link href="#link">Login</Nav.Link>
+                  <Nav.Link href="#link">Sign Up</Nav.Link>
+                  <Nav.Link href="#link">Logout</Nav.Link>
                 </Nav>
               </Navbar.Collapse>
             </Container>
