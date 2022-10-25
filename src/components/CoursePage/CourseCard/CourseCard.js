9b19747f8c30b36dbf5abcee87c8624af14a8596
Taslim <taslimahmed313@gmil.com>
2022-10-25 23:38:47 +0600

    Display Single Card in CardDetails

--- src/components/CoursePage/CourseCard/CourseCard.js
+++ src/components/CoursePage/CourseCard/CourseCard.js
@@ -1,23 +1,30 @@
 import React from 'react';
 import { Image } from 'react-bootstrap';
+import { FaCheckCircle } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
 import './CourseCard.css';
 
 const CourseCard = ({course}) => {
-    const {caption, img, description, category_id} = course;
+    const { caption, img, description, category_id, outline } = course;
     return (
-          <div className="course-card">
-            <Link to={`/course/${category_id}`}>
-              <Image
-                style={{ width: "100%", height: "200px", borderRadius: "8px" }}
-                src={img}
-              ></Image>
-            </Link>
-            <h5>{caption}</h5>
-            <p>
-              <small>{description}</small>
-            </p>
+      <div className="course-card">
+        <Link to={`/course/${category_id}`}>
+          <Image
+            style={{ width: "100%", height: "200px", borderRadius: "8px" }}
+            src={img}
+          ></Image>
+        </Link>
+        <h5>{caption}</h5>
+        <p>
+          <small>{description}</small>
+        </p>
+        {outline.map((learn) => (
+          <div className='text-start'>
+            <FaCheckCircle></FaCheckCircle>
+            <span>{learn}</span>
           </div>
+        ))}
+      </div>
     );
 };
 
--- src/components/CoursePage/CourseDetails/CourseDetails.js
+++ src/components/CoursePage/CourseDetails/CourseDetails.js
@@ -1,10 +1,28 @@
 import React from 'react';
+import { Button, Card, Image } from 'react-bootstrap';
+import { FaCheckCircle } from 'react-icons/fa';
+import { useLoaderData } from 'react-router-dom';
 
 const CourseDetails = () => {
+    const course = useLoaderData();
+    console.log(course);
+    const { caption, details, outline, img } = course;
     return (
-        <div>
-            <h1>This page is Course Details</h1>
-        </div>
+      <Card>
+        <Card.Header>Featured</Card.Header>
+        <Card.Body>
+            <Image src={img} style={{width:'100%'}}></Image>
+          <Card.Title>{caption}</Card.Title>
+          <Card.Text>
+            {details}
+          </Card.Text>
+          <FaCheckCircle></FaCheckCircle>
+          {
+            outline.map(learn=> <div><FaCheckCircle></FaCheckCircle><span>{learn}</span></div>)
+          }
+          <Button variant="primary">Go somewhere</Button>
+        </Card.Body>
+      </Card>
     );
 };
 
