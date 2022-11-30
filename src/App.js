b709b47365e7404a5e9f377872729eb23b69b72f
Taslim <taslimahmed313@gmil.com>
2022-11-30 18:32:57 +0600

    Set Max width

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import { router } from './Routes/Route/Route';
 function App() {
   
   return (
-        <div className="App">
+        <div className="App" style={{maxWidth: "1024px"}}>
           <RouterProvider router={router}></RouterProvider>
           <Toaster />
         </div>
